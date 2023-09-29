579b08b5762f3952492aa890fd51d36ed14d67d3
washington <washinglopes@hotmail.com>
2023-09-29 16:33:15 -0300

    Analisando candidatos completo

---
+++ src/ProcessoSeletivo.java
@@ -0,0 +1,17 @@
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        analisarCandidato(1900.0);
+        analisarCandidato(2200.0);
+        analisarCandidato(2000);
+    }
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+        if(salarioBase > salarioPretendido){
+            System.out.println("Ligar para o candidato");
+        }else if(salarioBase == salarioPretendido){
+            System.out.println("Ligar para o candidato com contra proposta");
+        }else{
+            System.out.println("Aguardando o resultado do demais canditatos");
+        }
+    }
+}
